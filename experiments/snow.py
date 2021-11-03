25af4581607b56802faa48d85f93a91f56cced5d
LM239 <larsmartin@hodne.net>
2021-11-03 16:58:36 +0100

    snow

--- experiments/snow.py
+++ experiments/snow.py
@@ -16,8 +16,7 @@ def ellipse_bbox(h, k, a, b, theta):
     vy = b * math.sin(theta + math.pi / 2)
     box_halfwidth = np.ceil(math.sqrt(ux ** 2 + vx ** 2))
     box_halfheight = np.ceil(math.sqrt(uy ** 2 + vy ** 2))
-    return ((int(h - box_halfwidth), int(k - box_halfheight))
-            , (int(h + box_halfwidth), int(k + box_halfheight)))
+    return (int(h + box_halfwidth), int(k + box_halfheight))
 
 
 # Rotated elliptical gradient - faster, vectorized numpy approach
@@ -35,12 +34,12 @@ def make_gradient_v2(width, height, h, k, a, b, theta):
 
 def draw_snow(a, b, theta):
     # Calculate the image size needed to draw this and center the ellipse
-    _, (h, k) = ellipse_bbox(0, 0, a, b, theta)  # Ellipse center
+    (h, k) = ellipse_bbox(0, 0, a, b, theta)  # Ellipse center
 
     width, height = (h * 2, k * 2)  # Canvas size
 
     # Generate the gradient and scale it to 8bit grayscale range
-    intensity = np.uint8(make_gradient_v2(width, height, h, k, a, b, theta) * 255)
+    intensity = np.uint8(make_gradient_v2(width, height, h, k, a, b, theta) * 255 * (0.2 + random.random() * 0.8))
 
     # Turn it into a BGRA image
     result = cv2.merge([intensity, intensity, intensity])
@@ -56,21 +55,25 @@ def add_snow(img):
     # Extracting the height and width of an image
     rows, cols = image.shape[:2]
 
-    snows = 50
+    snows = 2000
+    output = image.copy()
     for i in range(snows):
-        a, b = (random.randint(2, 4), random.randint(2, 4))  # Semi-major and semi-minor axis
+        a, b = (random.randint(2, 5), random.randint(2, 5))  # Semi-major and semi-minor axis
         theta = math.radians(90.0 * random.random())  # Ellipse rotation (radians)
 
         snow = draw_snow(a, b, theta)
         # displaying the orignal image
-        cv2.imshow('Original', snow)
-        # displaying the vignette filter image
-        cv2.waitKey(0)
+        x, y = random.randint(0, cols - snow.shape[1]), random.randint(0, rows - snow.shape[0])
+        w = np.ones_like(snow[:, :, 0])
+        for i in range(3):
+            weighted_square = output[y:y + snow.shape[0], x:x + snow.shape[1], i] * (w - snow[:,:,i]/255)
+            output[y:y + snow.shape[0], x:x + snow.shape[1], i] = np.add(weighted_square.astype(np.uint8), snow[:,:,i], dtype=np.uint8)
 
+    print(np.max(image))
     # displaying the orignal image
     cv2.imshow('Original', image)
     # displaying the vignette filter image
-    #cv2.imshow('VIGNETTE', output)
+    cv2.imshow('VIGNETTE', output)
     cv2.waitKey(0)
 
 if __name__ == "__main__":
